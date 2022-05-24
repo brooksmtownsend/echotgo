44b8ca0cb29a7fa40053f06900f05af894f33e41
Brooks Townsend <brooks@cosmonic.com>
2022-05-24 09:51:09 -0400

    initial commit

---
+++ echotgo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/wasmcloud/actor-tinygo"
+	"github.com/wasmcloud/interfaces/httpserver/tinygo"
+)
+
+func main() {
+	me := Echotgo{}
+	actor.RegisterHandlers(httpserver.HttpServerHandler(&me), actor.ActorHandler(&me))
+}
+
+type Echotgo struct{}
+
+func (e *Echotgo) HandleRequest(ctx *actor.Context, req httpserver.HttpRequest) (*httpserver.HttpResponse, error) {
+	r := httpserver.HttpResponse{
+		StatusCode: 200,
+		Header:     make(httpserver.HeaderMap, 0),
+		Body:       []byte("hello"),
+	}
+	return &r, nil
+}
+
+func (e *Echotgo) HealthRequest(ctx *actor.Context, arg actor.HealthCheckRequest) (*actor.HealthCheckResponse, error) {
+	var r actor.HealthCheckResponse
+	r.Healthy = true
+	return &r, nil
+}
